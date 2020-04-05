514f2616dd217e927651a4dd7101f87e4c2907c6
Leonardo Daza <leo.daza.769@gmail.com>
2020-04-05 06:08:02 -0400

    code updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Thermostat from './components/classes/thermostat'
 import Form from './components/classes/form'
 import Blog from './components/classes/blog'
 import LogControl from './components/classes/logcontrol'
-import Button from './classes/button'
+import Button from './components/classes/button'
 
 function App() {
   return (
